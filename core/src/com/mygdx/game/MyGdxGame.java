209238afa75478ff7e08cf392288b914bca8dbb8
riederch <rieder.christoph@gmx.at>
2016-04-20 10:52:06 +0200

    Beim Input den Gesture Listener Hinzugefügt

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -2,10 +2,13 @@ package com.mygdx.game;
 
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.InputMultiplexer;
 import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.input.GestureDetector;
+import com.badlogic.gdx.math.Vector2;
 
-public class MyGdxGame extends Game implements InputProcessor {
+public class MyGdxGame extends Game implements InputProcessor, GestureDetector.GestureListener {
 	private Game g;
 
 	public MyGdxGame(){
@@ -16,10 +19,16 @@ public class MyGdxGame extends Game implements InputProcessor {
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glBlendFunc(GL20.GL_SRC_ALPHA, GL20.GL_ONE_MINUS_SRC_ALPHA);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		Gdx.input.setInputProcessor((InputProcessor) this);
+
+		InputMultiplexer in=new InputMultiplexer();
+		in.addProcessor(new GestureDetector(this));
+		in.addProcessor(this);
+		Gdx.input.setInputProcessor(in);
+
 		g.setScreen(new MainMenueScreen(g));
 	}
 
+	// ------- Input Processor --------------
 	@Override
 	public boolean keyDown(int keycode) {
 		return false;
@@ -61,4 +70,48 @@ public class MyGdxGame extends Game implements InputProcessor {
 	public boolean scrolled(int amount) {
 		return false;
 	}
+
+	// ------- End Input Processor --------------
+
+	// ------------- Gesture Listener -------------
+	@Override
+	public boolean touchDown(float x, float y, int pointer, int button) {
+		return false;
+	}
+
+	@Override
+	public boolean tap(float x, float y, int count, int button) {
+		return false;
+	}
+
+	@Override
+	public boolean longPress(float x, float y) {
+		return false;
+	}
+
+	@Override
+	public boolean fling(float velocityX, float velocityY, int button) {
+		return false;
+	}
+
+	@Override
+	public boolean pan(float x, float y, float deltaX, float deltaY) {
+		return false;
+	}
+
+	@Override
+	public boolean panStop(float x, float y, int pointer, int button) {
+		return false;
+	}
+
+	@Override
+	public boolean zoom(float initialDistance, float distance) {
+		return false;
+	}
+
+	@Override
+	public boolean pinch(Vector2 initialPointer1, Vector2 initialPointer2, Vector2 pointer1, Vector2 pointer2) {
+		return false;
+	}
+	// ------------- End Gesture Listener -------------
 }
