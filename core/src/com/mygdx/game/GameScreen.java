e13803e7cf438626ba8998c7a4afef56db2d432e
Patrick <p.eisenkeil@gmail.com>
2016-04-21 18:07:20 +0200

    Ränder funktionieren mit Zoomfunktion .aber noch Probleme bei "Fingerwechsel"

--- core/src/com/mygdx/game/GameScreen.java
+++ core/src/com/mygdx/game/GameScreen.java
@@ -1,32 +1,27 @@
 package com.mygdx.game;
 
-import com.badlogic.gdx.Application;
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.InputMultiplexer;
 import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.Screen;
-import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.input.GestureDetector;
-import com.badlogic.gdx.maps.MapProperties;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TiledMapRenderer;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
-import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.math.Vector3;
-
-import sun.rmi.runtime.Log;
 
 /**
  * Created by Patrick on 14.04.2016.
  */
-public class GameScreen implements Screen, InputProcessor, GestureDetector.GestureListener  {
+public class GameScreen implements Screen, InputProcessor, GestureDetector.GestureListener {
     SpriteBatch batch;
     Texture img;
     TiledMap tiledMap;
@@ -57,7 +52,11 @@ public class GameScreen implements Screen, InputProcessor, GestureDetector.Gestu
         camera.setToOrtho(false, w, h);
         camera.update();
         tiledMapRenderer = new OrthogonalTiledMapRenderer(tiledMap);
-        Gdx.input.setInputProcessor((InputProcessor) this);
+        InputMultiplexer in = new InputMultiplexer();
+        in.addProcessor(new GestureDetector(this));
+        in.addProcessor(this);
+        Gdx.input.setInputProcessor(in);
+
         sr = new ShapeRenderer();
     }
 
@@ -127,40 +126,64 @@ public class GameScreen implements Screen, InputProcessor, GestureDetector.Gestu
 
     @Override
     public boolean touchDragged(int screenX, int screenY, int pointer) {
-        if(pointer==0) {
-            Vector2 newTouch = new Vector2(screenX, screenY);
-            Vector2 delta = newTouch.cpy().sub(lastTouch);
-            float xmin = w / 2;
-            float xmax = mapwidth - (w / 2);
-            float ymin = h / 2;
-            float ymax = mapheight - (h / 2);
+        Vector2 newTouch = new Vector2(screenX, screenY);
+        Vector2 delta = newTouch.cpy().sub(lastTouch);
+        boolean lockx = false;
+        boolean locky = false;
+
+        if (screenX == -1 && screenY == -1) {
+            delta.x = 0;
+            delta.y = 0;
+            newTouch = lastTouch;
+        }
+
+        if (pointer == 0 && Math.abs(delta.x) < 100 && Math.abs(delta.y) < 100) {
+            float xmin = (w / 2) * camera.zoom;
+            float xmax = mapwidth - ((w / 2) * camera.zoom);
+            float ymin = (h / 2) * camera.zoom;
+            float ymax = mapheight - ((h / 2) * camera.zoom);
 
             Vector3 oldpos = camera.position;
+
+            if (ymin >= ymax) {
+                locky = true;
+                delta.y = (mapheight / 2) - camera.position.y;
+            }
+            if (xmin >= xmax) {
+                lockx = true;
+                delta.x = -((mapwidth / 2) - camera.position.x);
+            }
+
             Vector3 newpos = new Vector3(oldpos.x - delta.x, oldpos.y + delta.y, 0);
 
-            if (newpos.x <= xmin) {
-                if (delta.x > 0) {
-                    delta.x = delta.x - (xmin - newpos.x);
-                }
-            } else if (newpos.x >= (mapwidth - (w / 2))) {
-                if (delta.x < 0) {
-                    delta.x = delta.x - (xmax - newpos.x);
-                }
-            }
-
-            if (camera.position.y <= ymin) {
-                if (delta.y < 0) {
-                    delta.y = delta.y + (ymin - newpos.y);
-                }
-            } else if (camera.position.y >= ymax) {
-                if (delta.y > 0) {
-                    delta.y = delta.y + (ymax - newpos.y);
+            if (lockx == false) {
+                if (newpos.x <= xmin) {
+                    if (delta.x > 0) {
+                        delta.x = delta.x - (xmin - newpos.x);
+                    }
+                } else if (newpos.x >= xmax) {
+                    if (delta.x < 0) {
+                        delta.x = delta.x - (xmax - newpos.x);
+                    }
                 }
             }
 
-            camera.translate(-delta.x, delta.y);
-            lastTouch = newTouch;
+            if (locky == false) {
+                if (camera.position.y <= ymin) {
+                    if (delta.y < 0) {
+                        delta.y = delta.y + (ymin - newpos.y);
+                    }
+                } else if (camera.position.y >= ymax) {
+                    if (delta.y > 0) {
+                        delta.y = delta.y + (ymax - newpos.y);
+                    }
+                }
+            }
         }
+        camera.translate(-delta.x, delta.y);
+        newTouch.x=screenX;
+        newTouch.y=screenY;
+        lastTouch = newTouch;
         return true;
     }
 
@@ -207,13 +230,23 @@ public class GameScreen implements Screen, InputProcessor, GestureDetector.Gestu
 
     @Override
     public boolean zoom(float initialDistance, float distance) {
-        Gdx.app.log("ZOOM", "ZOOM");
+        float dist = (initialDistance - distance) / (camera.zoom);
+        while (Math.abs(dist) > 0.05) {
+            dist = dist / 5;
+        }
+        if ((camera.zoom + dist) < 1) {
+            camera.zoom = 1;
+        } else if ((camera.zoom + dist) > 5) {
+            camera.zoom = 5;
+        } else {
+            camera.zoom = camera.zoom + dist;
+        }
+        touchDragged(-1, -1, 0);
         return true;
     }
 
     @Override
     public boolean pinch(Vector2 initialPointer1, Vector2 initialPointer2, Vector2 pointer1, Vector2 pointer2) {
-        Gdx.app.log("ZOOM", "ZOOM");
-        return true;
+        return false;
     }
 }
