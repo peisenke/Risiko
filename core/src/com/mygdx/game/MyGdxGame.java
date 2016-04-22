bfd4b7a5c5ac2c81e5bf34631ca5ace7e4435827
Patrick <p.eisenkeil@gmail.com>
2016-04-22 15:41:59 +0200

    Mergr Aufräumarbeiten

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -7,13 +7,9 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.input.GestureDetector;
 import com.badlogic.gdx.math.Vector2;
 
-<<<<<<< HEAD
 
 public class MyGdxGame extends Game{
-        parent of 4a0a037... Revert "test1"
-=======
-public class MyGdxGame extends Game{
->>>>>>> refs/remotes/origin/Countries
+
 	private Game g;
 
 	public MyGdxGame(){
@@ -23,10 +19,7 @@ public class MyGdxGame extends Game{
 	public void create() {
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glBlendFunc(GL20.GL_SRC_ALPHA, GL20.GL_ONE_MINUS_SRC_ALPHA);
-<<<<<<< HEAD
-=======
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
->>>>>>> refs/remotes/origin/Countries
 		g.setScreen(new MainMenueScreen(g));
 	}
 }
