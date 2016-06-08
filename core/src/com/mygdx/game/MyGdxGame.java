01d6f49372d6dfe4cd6076e25a9aa71f5fbed082
peisenke <p.eisenkeil@gmail.com>
2016-06-08 06:58:32 +0200

    Menü und Musik

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -3,6 +3,7 @@ package com.mygdx.game;
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.InputProcessor;
+import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.input.GestureDetector;
 import com.badlogic.gdx.math.Vector2;
@@ -13,6 +14,8 @@ public class MyGdxGame extends Game{
 	private MyGdxGame g;
 	private Preferences pref=new Preferences();
 	Player p=new Player();
+	Music music;
+
 
 	public MyGdxGame(){
 		g=this;
@@ -22,6 +25,10 @@ public class MyGdxGame extends Game{
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glBlendFunc(GL20.GL_SRC_ALPHA, GL20.GL_ONE_MINUS_SRC_ALPHA);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		music = Gdx.audio.newMusic(Gdx.files.internal("Sound/loop1.mp3"));
+		music.setVolume(pref.getMusic()/100);                 // sets the volume to half the maximum volume
+		music.setLooping(true);                // will repeat playback until music.stop() is called
+		music.play();
 		g.setScreen(new MainMenueScreen(g));
 	}
 
@@ -39,5 +46,9 @@ public class MyGdxGame extends Game{
 
 	public void setP(Player p) {
 		this.p = p;
+	}
+
+	public Music getMusic() {
+		return music;
 	}
 }
