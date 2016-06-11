2303efa19ec9ee7ebb574f8eda9908497fe86424
peisenke <p.eisenkeil@gmail.com>
2016-06-11 18:25:01 +0200

    Netzwerk join und host fertig

--- core/src/com/mygdx/game/GameScreen.java
+++ core/src/com/mygdx/game/GameScreen.java
@@ -100,7 +100,7 @@ public class GameScreen implements Screen, GestureDetector.GestureListener {
         tiledMapRenderer = new OrthogonalTiledMapRenderer(tiledMap);
 
         // init Polygons
-
+        objectsBatch = new PolygonSpriteBatch();
         hud = new HudLayer((float)w, (float)h,gl);
 
         in.addProcessor(new GestureDetector(this));
