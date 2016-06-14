f20f19b1cb685dccca3292da75c6e83320e58f3b
Ermin Muratovic <Ermin Muratovic>
2016-06-14 20:24:07 +0200

    Create GameLogicTest

---
+++ android/src/test/java/com/mygdx/game/GameLogicTest.java
@@ -0,0 +1,92 @@
+package com.mygdx.game;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by ErminiFatima on 14.06.2016.
+ */
+public class GameLogicTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @Test
+    public void testReinforce() throws Exception {
+
+    }
+
+    @Test
+    public void testAttack() throws Exception {
+
+    }
+
+    @Test
+    public void testMove() throws Exception {
+
+    }
+
+    @Test
+    public void testPhaseup() throws Exception {
+
+    }
+
+    @Test
+    public void testCountdown() throws Exception {
+
+    }
+
+    @Test
+    public void testGetTime() throws Exception {
+
+    }
+
+    @Test
+    public void testSetTime() throws Exception {
+
+    }
+
+    @Test
+    public void testGetGs() throws Exception {
+
+    }
+
+    @Test
+    public void testSetGs() throws Exception {
+
+    }
+
+    @Test
+    public void testGetFirstcntry() throws Exception {
+
+    }
+
+    @Test
+    public void testSetFirstcntry() throws Exception {
+
+    }
+
+    @Test
+    public void testGetSecondcntry() throws Exception {
+
+    }
+
+    @Test
+    public void testSetSecondcntry() throws Exception {
+
+    }
+
+    @Test
+    public void testGetGamsc() throws Exception {
+
+    }
+
+    @Test
+    public void testSetGamsc() throws Exception {
+
+    }
+}
